20a91945e223bc5c2d6d5750869ac1b7cf432f1e
Sujay Sayini <sujaysayini@gmail.com>
2020-05-02 22:42:55 -0400

    third

--- WTF.c
+++ WTF.c
@@ -3,7 +3,191 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <math.h>
+#include <limits.h>
+#include <libgen.h>
+#include <dirent.h>
+#include <netdb.h> 
 
-int main(){
+#define true 1
+#define false 0
+
+int string_equal(char *arg1, char *arg2)
+{
+    int a = 0;
+    //We loop until both equal the null terminator
+    //The two strings will differ either in character or length before
+    //we hit an out of bounds
+    while (arg1[a] != '\0' || arg2[a] != '\0')
+    {
+        if (arg1[a] != arg2[a]){
+            return false;
+        }
+        a++;
+    }
+    return true;
+}
+void configure(char* IP_address, char* port){
+    int fd = open(".configure", O_RDWR | O_CREAT | O_TRUNC, 00744);
+    //strcat(IP_address, " ");
+    write(fd,IP_address, sizeof(IP_address));
+    write(fd," ", 1);
+    write(fd,port, sizeof(port));
+    write(fd,"\n",1);
+    close(fd);
+}
+
+void checkout(int sockfd, char* project_name){
+
+
+}
+
+void create(int sockfd, char* project_name){
+    
+
+}
+void add(int sockfd, char* project_name, char* file_name){
+
+
+}
+void removefd(int sockfd, char* project_name, char* file_name){
+
+
+}
+
+int main(int argc, char **argv){
+    // client side 
+
+    if(string_equal(argv[1], "configure")){
+        configure(argv[2],argv[3]);
+        return 0;
+    }
+    //checking if configure exists
+    int fd = open("./.configure", O_RDWR, 00744);
+    if(fd < 0){
+        printf("Configure doesn't exist, run configure command before any other command.\n");
+        return -1;  
+    }
+    
+    char ip[20];
+    char port[6];
+
+    int read_status = 0;
+    char c = ' ';
+    int i = 0;
+    char buffer[100];    
+    
+    do {
+        printf("buffer is = %s\n", buffer);
+        read_status = read(fd, &c, 1);
+        if(read_status == 0){ // reached the end of the file
+            break;
+        }
+
+        if(c == ' '){
+            buffer[i] = '\0';
+            strcpy(ip,buffer);
+            memset(buffer, '\0', 100);
+            i = 0;
+        }
+        else {
+            buffer[i] = c;
+            i++;
+        }
+
+    }while(read_status > 0);
+
+    buffer[i] = '\0';
+    strcpy(port,buffer);
+
+    int port_in_int = atoi(port);
+    if(port_in_int < 0){
+        printf("Invalid number, please try again.\n");
+        return -1;
+    }
+
+    //printf("ip is %s and port is %d\n", ip, port_in_int);
+    
+    // Now creating our connection to the server
+    int sockfd; 
+    struct sockaddr_in servaddr; 
+  
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("Creating Socket failed\n"); 
+        return -1;
+    } else{
+        printf("Socket successfully created.\n"); 
+    }
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = inet_addr(ip); 
+    servaddr.sin_port = htons(port_in_int); 
+
+    if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) { 
+        printf("Connection with the server failed.\n"); 
+        return -1;
+    } 
+    else{
+        printf("Connected to the server.\n"); 
+    }
+
+    if (string_equal(argv[1], "checkout")){ 
+        int project_fd = open(argv[2],O_RDWR,744);
+        if(project_fd>0){
+            printf("Project already exists on client side.");
+            return -1;
+        }
+        checkout(sockfd, argv[2]);
+
+    } else if (string_equal(argv[1], "update")){
+
+    } else if (string_equal(argv[1], "upgrade")){
+
+    } else if (string_equal(argv[1], "commit")){
+
+    } else if (string_equal(argv[1], "push")){
+
+    } else if (string_equal(argv[1], "create")){
+        int project_fd = open(argv[2],O_RDWR,744);
+        if(project_fd>0){
+            printf("Project already exists on client side.");
+            return -1;
+        }
+        create(sockfd,argv[2]);
+
+    } else if (string_equal(argv[1], "destroy")){
+
+    } else if (string_equal(argv[1], "add")){
+        add(sockfd, argv[2], argv[3]);
+
+    } else if (string_equal(argv[1], "remove")){
+        removefd(sockfd, argv[2], argv[3]);
+
+    } else if (string_equal(argv[1], "currentversion")){
+
+    } else if (string_equal(argv[1], "history")){
+
+    } else if (string_equal(argv[1], "rollback")){
+        int version = atoi(argv[3]);
+        if(version == 0){
+            printf("Invalid number, please try again.\n");
+            return -1;
+        }
+
+    } else {
+        printf("Invalid command, please try again.\n");
+        return -1;
+    }
+
+    close(sockfd);
     return 0;
 }
--- WTFserver.c
+++ WTFserver.c
@@ -6,8 +6,6 @@
 #include <sys/socket.h> 
 #include <sys/types.h> 
 #define MAX 80 
-#define PORT 8080 
-#define SA struct sockaddr 
   
 // Function designed for chat between client and server. 
 void func(int sockfd) 
@@ -39,56 +37,63 @@ void func(int sockfd)
     } 
 } 
   
-// Driver function 
-int main() 
-{ 
+
+int main(int argc, char **argv){
+
+    int port = atoi(argv[1]);
+
     int sockfd, connfd, len; 
     struct sockaddr_in servaddr, cli; 
   
-    // socket create and verification 
     sockfd = socket(AF_INET, SOCK_STREAM, 0); 
     if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully created..\n"); 
+        printf("Socket creation failed.\n"); 
+        return 0;
+    } else{
+        printf("Socket successfully created.\n"); 
+    }
+    
     bzero(&servaddr, sizeof(servaddr)); 
   
-    // assign IP, PORT 
     servaddr.sin_family = AF_INET; 
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(PORT); 
+    servaddr.sin_port = htons(port); 
   
     // Binding newly created socket to given IP and verification 
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-        printf("socket bind failed...\n"); 
+    if ((bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) { 
+        printf("Socket bind failed.\n"); 
         exit(0); 
     } 
-    else
-        printf("Socket successfully binded..\n"); 
+    else{
+        printf("Socket successfully binded..\n");
+    } 
   
     // Now server is ready to listen and verification 
     if ((listen(sockfd, 5)) != 0) { 
         printf("Listen failed...\n"); 
-        exit(0); 
+        return -1;
     } 
-    else
+    else{
         printf("Server listening..\n"); 
+    }
+    
+    
     len = sizeof(cli); 
-  
-    // Accept the data packet from client and verification 
-    connfd = accept(sockfd, (SA*)&cli, &len); 
+  // ------------------------------ multithreading ---------
+    connfd = accept(sockfd, (struct sockaddr *)&cli, (socklen_t*) &len); 
     if (connfd < 0) { 
         printf("server acccept failed...\n"); 
-        exit(0); 
+        return -1;
     } 
-    else
-        printf("server acccept the client...\n"); 
+    else{
+        printf("server acccept the client...\n");
+    } 
   
     // Function for chatting between client and server 
     func(connfd); 
+
+    // -------------------------------------------------
   
-    // After chatting close the socket 
     close(sockfd); 
+    return 0;
 } 
